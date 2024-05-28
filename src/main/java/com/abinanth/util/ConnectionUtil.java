0f5e58b97a5d304b1b2297d190cf97026b591b72
abinanth <abinanth1998@gmail.com>
2024-05-28 21:47:45 +0530

    first commit

--- src/main/java/com/abinanth/util/ConnectionUtil.java
+++ src/main/java/com/abinanth/util/ConnectionUtil.java
@@ -10,14 +10,17 @@ public class ConnectionUtil {
 
 	}
 
-	private static String driverClass = System.getenv("spring.datasource.driver-class-name");
-	private static String url = System.getenv("spring.datasource.url");
-	private static String username = System.getenv("spring.datasource.username");
-	private static String password = System.getenv("spring.datasource.password");
+//	private static String driverClass = System.getenv("spring.datasource.driver-class-name");
+//	private static String url = System.getenv("spring.datasource.url");
+//	private static String username = System.getenv("spring.datasource.username");
+//	private static String password = System.getenv("spring.datasource.password");
 
+	//private static String driverClass = System.getenv("spring.datasource.driver-class-name");
+	private static String url = System.getenv("jdbc:postgresql://localhost:5432/app ");
+	private static String username = System.getenv("port");
+	private static String password = System.getenv("admin");
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-
-		Class.forName(driverClass);
+		//Class.forName(driverClass);
 		Connection connection = DriverManager.getConnection(url, username, password);
 
 		return connection;
